7e9e91cb868124faeb3f87d834d2b4e9b9f93560
xilengs <825047736@qq.com>
2025-09-17 17:25:56 +0800

    2025/9/17

--- AlexNet/train.py
+++ AlexNet/train.py
@@ -1,9 +1,12 @@
 import torch
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import os
 from PIL import Image
 from torchvision import transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 num_class = 2
 
@@ -12,7 +15,8 @@ class AlexNet(nn.Module):
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
-            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.BatchNorm2d(96),
+            # nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
             nn.ReLU(inplace=True),
             # 重叠池化
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -20,18 +24,22 @@ class AlexNet(nn.Module):
             # nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
 
             nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2),
-            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.BatchNorm2d(256),
+            # nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             # nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
 
             nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True),
 
             nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True),
 
             nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
@@ -76,6 +84,9 @@ class AlexDataset(Dataset):
         image = Image.open(image_path).convert("RGB")
 
         if self.transform:
+            # image = np.array(image)
+            # augmented = self.transform(image=image)
+            # image = augmented['image']
             image = self.transform(image)
 
         return image, label
@@ -92,6 +103,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
 
+    # transforms强化
     train_transform = transforms.Compose([
         transforms.Resize((227, 227)),
         transforms.RandomHorizontalFlip(p=0.5),
@@ -112,7 +124,30 @@ if __name__ == '__main__':
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-    train_dataset = AlexDataset(train_image_dir, train_label_txt, test_transform)
+
+    # Albumentations强化
+    train_A_transform = A.Compose([
+        A.Resize(227, 227),
+        A.HorizontalFlip(p=0.5),
+        A.ColorJitter(
+            brightness=0.5,
+            contrast=0.5,
+            saturation=0.5,
+            hue=0.1
+        ),
+        A.Rotate(limit=50),
+        A.Normalize(mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225)),
+        ToTensorV2()
+    ])
+    test_A_transform = A.Compose([
+        A.Resize(227, 227),
+        A.Normalize(mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225)),
+        ToTensorV2()
+    ])
+
+    train_dataset = AlexDataset(train_image_dir, train_label_txt, train_transform)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_dataset = AlexDataset(test_image_dir, test_label_txt, test_transform)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
